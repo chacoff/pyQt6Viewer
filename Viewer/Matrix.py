several fixes about the calculation of statistics and confusion matrix since the labels were inverted

Viewer/Matrix.py
@@ -4,14 +4,38 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import os
+sns.set_style("whitegrid")
 
 
-def create_matrix():
-    sns.set_style("whitegrid")
-    main_source = f'..'
-    source = f'{main_source}\\ZH.csv'
-    name = 'ZH018'  # (source.split('\\')[-1]).split('.')[0]
-    df = pd.read_csv(source)
+# TODO @Pierrick, please turn it into a class
+def create_matrix(image_path, matrix_csv):
+
+    df = pd.read_csv(matrix_csv)
+    df.index += 1
+
+    y_pred = df['Predict']
+    y_test = df['Ground_truth']
+    labels = y_pred.unique()
+    labels.sort()
+
+    cf_matrix = confusion_matrix(y_test, y_pred)
+
+    ax = sns.heatmap(cf_matrix/np.sum(cf_matrix), annot=True, fmt='.2%', cmap='Oranges')
+
+    ax.set_title('TMB Seams - Confusion Matrix')
+    ax.set_xlabel('Predicted Values')
+    ax.set_ylabel('Actual Values ')
+
+    ax.xaxis.set_ticklabels(labels)
+    ax.yaxis.set_ticklabels(labels)
+
+    # plt.savefig(image_path, bbox_inches='tight', dpi=199)
+    plt.show()
+    plt.clf()
+
+
+def print_a_brief(matrix_csv):
+    df = pd.read_csv(matrix_csv)
     df.index += 1
 
     y_pred = df['Ground_truth']
@@ -28,21 +52,4 @@ def create_matrix():
     test = pd.concat([c_test, p_test], axis=1, keys=['counts', '%'])
 
     brief = f'\nPREDICTIONS\n{pred}\n\nGROUND TRUTH\n{test}'
-
-
-    cf_matrix = confusion_matrix(y_test, y_pred)
-    # print(cf_matrix)
-
-    # ax = sns.heatmap(cf_matrix, annot=True, cmap='Blues', fmt='g')
-    ax = sns.heatmap(cf_matrix/np.sum(cf_matrix), annot=True, fmt='.2%', cmap='Oranges')
-
-    ax.set_title('TMB Seams - Confusion Matrix')
-    ax.set_xlabel('Predicted Values')
-    ax.set_ylabel('Actual Values ')
-
-    ax.xaxis.set_ticklabels(labels)
-    ax.yaxis.set_ticklabels(labels)
-
-    # plt.show()
-    lastname = os.path.join('matrix', 'confusion_matrix.png')
-    plt.savefig(lastname, bbox_inches='tight', dpi=199)
+    print(brief)