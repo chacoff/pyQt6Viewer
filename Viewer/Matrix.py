5a4f50d1bba5f2c6168561d420068dd40ece0291
EUROPE\CHEVIRPI <pierrick.cheviron@arcelormittal.com>
2023-07-03 16:27:17 +0200

    [ADD] TODO @Pierrick, please turn into a class (make the matrix function work inside of a class) [ADD] TODO Zoom in and out according mouse pointer (add zoom by selecting a zone)

--- Viewer/Matrix.py
+++ Viewer/Matrix.py
@@ -4,51 +4,55 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import os
+
 sns.set_style("whitegrid")
 
 
 # TODO @Pierrick, please turn it into a class
-def create_matrix(image_path, matrix_csv):
+class MatrixCreationClass:
+    def __init__(self, matrix_csv, image_path):
+        self.matrix_csv = matrix_csv
+        self.image_path = image_path
 
-    df = pd.read_csv(matrix_csv)
-    df.index += 1
+    def create_matrix(self):
+        df = pd.read_csv(self.matrix_csv)
+        df.index += 1
 
-    y_pred = df['Predict']
-    y_test = df['Ground_truth']
-    labels = y_pred.unique()
-    labels.sort()
+        y_pred = df['Predict']
+        y_test = df['Ground_truth']
+        labels = y_pred.unique()
+        labels.sort()
 
-    cf_matrix = confusion_matrix(y_test, y_pred)
+        cf_matrix = confusion_matrix(y_test, y_pred)
 
-    ax = sns.heatmap(cf_matrix/np.sum(cf_matrix), annot=True, fmt='.2%', cmap='Oranges')
+        ax = sns.heatmap(cf_matrix / np.sum(cf_matrix), annot=True, fmt='.2%', cmap='Oranges')
 
-    ax.set_title('TMB Seams - Confusion Matrix')
-    ax.set_xlabel('Predicted Values')
-    ax.set_ylabel('Actual Values ')
+        ax.set_title('TMB Seams - Confusion Matrix')
+        ax.set_xlabel('Predicted Values')
+        ax.set_ylabel('Actual Values ')
 
-    ax.xaxis.set_ticklabels(labels)
-    ax.yaxis.set_ticklabels(labels)
+        ax.xaxis.set_ticklabels(labels)
+        ax.yaxis.set_ticklabels(labels)
 
-    plt.savefig(image_path, bbox_inches='tight', dpi=199)
-    plt.clf()
+        plt.savefig(self.image_path, bbox_inches='tight', dpi=199)
+        plt.clf()
 
+    def print_a_brief(self):
+        df = pd.read_csv(self.matrix_csv)
+        df.index += 1
 
-def print_a_brief(matrix_csv):
-    df = pd.read_csv(matrix_csv)
-    df.index += 1
+        y_pred = df['Ground_truth']
+        y_test = df['Predict']
+        labels = y_test.unique()
+        labels.sort()
 
-    y_pred = df['Ground_truth']
-    y_test = df['Predict']
-    labels = y_test.unique()
-    labels.sort()
+        c_pred = y_pred.value_counts(dropna=False)
+        p_pred = y_pred.value_counts(dropna=False, normalize=True).mul(100).round(2)
+        pred = pd.concat([c_pred, p_pred], axis=1, keys=['counts', '%'])
 
-    c_pred = y_pred.value_counts(dropna=False)
-    p_pred = y_pred.value_counts(dropna=False, normalize=True).mul(100).round(2)
-    pred = pd.concat([c_pred, p_pred], axis=1, keys=['counts', '%'])
+        c_test = y_test.value_counts(dropna=False)
+        p_test = y_test.value_counts(dropna=False, normalize=True).mul(100).round(2)
+        test = pd.concat([c_test, p_test], axis=1, keys=['counts', '%'])
 
-    c_test = y_test.value_counts(dropna=False)
-    p_test = y_test.value_counts(dropna=False, normalize=True).mul(100).round(2)
-    test = pd.concat([c_test, p_test], axis=1, keys=['counts', '%'])
-
-    brief = f'\nPREDICTIONS\n{pred}\n\nGROUND TRUTH\n{test}'
-    print(brief)
+        brief = f'\nPREDICTIONS\n{pred}\n\nGROUND TRUTH\n{test}'
+        print(brief)
