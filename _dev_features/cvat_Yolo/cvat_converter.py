7f8513f73577cc5c85e8dc3e0121ceb78be5bca6
Gomez Chacoff, Jaime <jaime.gomez-chacoff@arcelormittal.com>
2023-08-18 15:52:10 +0200

    modifications in the data handling

--- _dev_features/cvat_Yolo/cvat_converter.py
+++ _dev_features/cvat_Yolo/cvat_converter.py
@@ -10,10 +10,12 @@ class CVATYoloConverter:
         self.classes_encoding: dict = classes_encoding
         self.label: int = 0
 
-    def get_image_annotation(self) -> None:
+    def get_image_annotation(self, _xml_file: str) -> None:
         """ XML in CVAT format to YOLO annotations """
 
-        root = ET.parse(self.xml_path_list).getroot()
+        rel_path = os.path.dirname(_xml_file)
+
+        root = ET.parse(_xml_file).getroot()
         meta = root.findall("image")
 
         for image in meta:
@@ -42,11 +44,20 @@ class CVATYoloConverter:
                 yolo_w = round((xbr - xtl) / image_width, 6)
                 yolo_h = round((ybr - ytl) / image_height, 6)
 
-                payload = [image_name, f'{self.label} {yolo_x} {yolo_y} {yolo_w} {yolo_h}']
+                full_image_name = os.path.join(rel_path, image_name)
+                payload = [full_image_name, f'{self.label} {yolo_x} {yolo_y} {yolo_w} {yolo_h}']
                 self.write_annotations_to_disk(payload)
 
     def classes_encoder(self, label) -> int:
         return self.classes_encoding[label]
+
+    def find_xml_files_recursive(self) -> list:
+        _xml_files: list = []
+        for root, dirs, files in os.walk(self.xml_path_list):
+            for file in files:
+                if file.endswith('.xml'):
+                    _xml_files.append(os.path.join(root, file))
+        return _xml_files
 
     @staticmethod
     def points_to_points_array(points) -> np.array:
@@ -72,7 +83,8 @@ class CVATYoloConverter:
 
 
 def main():
-    xml_dir: str = 'annotations.xml'
+    xml_dir: str = r'C:\Users\gomezja\PycharmProjects\00_dataset\training'
+
     classes_encoding: dict = {
         'Seams': 0,
         'Beam': 1,
@@ -82,7 +94,14 @@ def main():
     }
 
     convert = CVATYoloConverter(xml_dir, classes_encoding)
-    convert.get_image_annotation()
+    xmls = convert.find_xml_files_recursive()
+
+    i = 0
+    for xml in xmls:
+        print(xml)
+        convert.get_image_annotation(xml)
+        i += 1
+    print(f'found {i} annotations.xml')
 
 
 if __name__ == '__main__':
--- _dev_features/cvat_Yolo/cvat_noannotations.py
+++ _dev_features/cvat_Yolo/cvat_noannotations.py
@@ -1,12 +1,81 @@
 import os
+import shutil
+from collections import Counter
+
+def no_lot_seeker(folder: str):
+    for file in os.listdir(folder):
+        if file.endswith('.png'):
+            print(file)
+        elif file.endswith('.bmp'):
+            print(file)
+        elif file.endswith('.xml'):
+            pass
 
 
-folder = r'C:\Users\gomezja\PycharmProjects\00_dataset\training'
+def count_images(folder_path, extensions):
+    bmp_count = 0
+    png_count = 0
 
-for file in os.listdir(folder):
-    if file.endswith('.png'):
-        print(file)
-    elif file.endswith('.bmp'):
-        print(file)
-    elif file.endswith('.xml'):
-        pass
+    for root, _, files in os.walk(folder_path):
+        for file in files:
+            _, ext = os.path.splitext(file)
+            if ext.lower() in extensions:
+                if ext.lower() == ".bmp":
+                    bmp_count += 1
+                elif ext.lower() == ".png":
+                    png_count += 1
+
+    return bmp_count, png_count
+
+
+def find_and_copy_missing_images(uploaded_folder, dataset_folder, missing_folder):
+    uploaded_images = get_image_list_recursive(uploaded_folder)
+    print(f'uploaded: {len(uploaded_images)}')
+    dataset_images = get_image_list_recursive(dataset_folder)
+    print(f'dataset: {len(dataset_images)}')
+
+    missing_images = [x for x in uploaded_images if x not in dataset_images]
+    print(f'missing: {len(missing_images)}')
+
+    duplicate_items = find_duplicates(uploaded_images)
+    print(len(duplicate_items))
+
+    # for image_path in missing_images:
+    #     source_image_path = os.path.join(uploaded_folder, image_path)
+    #     missing_image_path = os.path.join(missing_folder, image_path)
+    #
+    #     # Create necessary folders in the missing directory
+    #     missing_image_folder = os.path.dirname(missing_image_path)
+    #     os.makedirs(missing_image_folder, exist_ok=True)
+    #
+    #     print(f"Copying missing image {image_path} to missing folder.")
+    #     # shutil.copy(source_image_path, missing_image_path)
+
+
+def get_image_list_recursive(folder):
+    image_list = []
+    for root, _, files in os.walk(folder):
+        for file in files:
+            if file.lower().endswith(('.png', '.bmp')):
+                image_list.append(os.path.join(file))
+    return image_list
+
+
+def find_duplicates(lst):
+    counter = Counter(lst)
+    duplicates = [item for item, count in counter.items() if count > 1]
+    return duplicates
+
+
+if __name__ == '__main__':
+
+    uploaded = r'C:\Users\gomezja\PycharmProjects\00_dataset\training_uploaded'
+    dataset = r'C:\Users\gomezja\PycharmProjects\00_dataset\training'
+    missing = r'C:\Users\gomezja\PycharmProjects\00_dataset\training\missing'
+    extensions = [".bmp", ".png"]
+
+    bmp_count, png_count = count_images(dataset, extensions)
+    print(f"Number of BMP images: {bmp_count}\nNumber of PNG images: {png_count}")
+
+    find_and_copy_missing_images(uploaded, dataset, missing)
+
