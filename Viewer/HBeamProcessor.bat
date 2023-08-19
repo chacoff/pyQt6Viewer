4951359782449fcc54da26aa017e2b0f703ce7c9
Gomez Chacoff, Jaime <jaime.gomez-chacoff@arcelormittal.com>
2023-08-19 21:34:48 +0200

    update starter

--- Viewer/HBeamProcessor.bat
+++ Viewer/HBeamProcessor.bat
@@ -6,7 +6,7 @@ SETLOCAL EnableDelayedExpansion
 	:JAIME
     if "%COMPUTERNAME%" == "5CG1436PDJ" (
         echo "Jaime PC"
-        SET CONDA=C:\Users\gomezja\Anaconda3
+        SET CONDA=C:\Users\gomezja\miniconda3
         SET LOCATION=C:\Users\gomezja\PycharmProjects\202_SeamsProcessing\Viewer\
         goto LAUNCH
     ) else (
@@ -39,7 +39,9 @@ timeout /t 2 >nul
 
 	SET CONDA_1=C:\Users\%USERNAME%\Anaconda3
 	SET CONDA_2=C:\Users\%USERNAME%\miniconda3
-	SET PATH=%CONDA_1%\Scripts;%CONDA_1%\envs\seams;%CONDA_2%\Scripts;%CONDA_2%\envs\seams;%PATH%
+	SET CONDA_3=C:\Users\%USERNAME%\AppData\Local\miniconda3
+	SET CONDA_4=C:\Users\%USERNAME%\AppData\Local\Anaconda3
+	SET PATH=%CONDA_1%\Scripts;%CONDA_1%\envs\seams;%CONDA_2%\Scripts;%CONDA_2%\envs\seams;;%CONDA_3%\Scripts;%CONDA_3%\envs\seams;;%CONDA_4%\Scripts;%CONDA_4%\envs\seams;%PATH%
 
 	if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
 		CALL activate seams
